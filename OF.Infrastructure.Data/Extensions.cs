b6881b7e55c10e2c50bcb8ea5c1a639aa3b113bd
jidexl21 <jidexl21@gmail.com>
2021-07-10 11:39:21 +0100

    Added Extensions

---
+++ OF.Infrastructure.Data/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OF.Infrastructure.Data
+{
+    public static class Extensions
+    {
+        public static async Task ForEachAsync<T>(this List<T> list, Func<T, Task> func)
+        {
+            foreach (var value in list)
+            {
+                func(value).Wait(10000);
+            }
+        }
+
+        public static string CreateMD5(this string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        public static string Btoa(this string toEncode)
+        {
+            byte[] bytes = Encoding.GetEncoding(28591).GetBytes(toEncode);
+            string toReturn = System.Convert.ToBase64String(bytes);
+            return toReturn;
+        }
+
+        public static bool IsCollection(this object item) => item.GetType().GetInterface("IList") != null;
+        public static object GetValueX(this System.Reflection.PropertyInfo item, object parentItem) =>
+            (item.PropertyType.IsEnum) ? item.GetValue(parentItem).ToString() : item.GetValue(parentItem);
+    }
+}
