19cdefe7526a65813e1c39aaafdbe3bae2b66938
jidexl21 <jidexl21@gmail.com>
2021-08-07 15:52:45 +0100

    Added OF Auth

---
+++ OF.Infrastructure.Auth/DisallowAnonymousAttribute.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OF.Auth.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace OF.Infrastructure.Auth
+{
+    public class DisallowAnonymousAttribute : ActionFilterAttribute
+    {
+        private readonly string[] requiredRoles;
+        /// <summary>
+        /// 
+        /// </summary>
+        public DisallowAnonymousAttribute()
+        {
+            requiredRoles = new string[] { };
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="roles">A comma delimited list of RoleCodes allowed on this endpoint</param>
+        public DisallowAnonymousAttribute(string roles)
+        {
+            requiredRoles = roles.ToUpper().Split(',');
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Items.ContainsKey("User"))
+            {
+                filterContext.Result = new ContentResult() { Content = "Request is not authorized. Login required", StatusCode = (int)HttpStatusCode.Unauthorized }; ;
+            }
+            var usr = (AppUser)filterContext.HttpContext.Items["User"];
+
+            bool hsrole = (usr.UserRoles.Select(x => x.Code).Intersect(requiredRoles.Distinct()).ToList().Distinct().Count() == requiredRoles.Length);
+
+            if (!hsrole) { filterContext.Result = new ContentResult() { Content = "Insufficent Permissions", StatusCode = (int)HttpStatusCode.Unauthorized }; };
+            //if (filterContext.HttpContext.Session != null)
+            //{
+            //    //var user = filterContext.HttpContext.Session["User"] as User;
+            //    //if (user != null && string.IsNullOrEmpty(user.FirstName))
+            //    //    filterContext.Result = new RedirectResult("/home/firstname");
+            //    //else
+            //    //{
+            //    //    //what ever you want, or nothing at all
+            //    //}
+            //}
+        }
+    }
+
+}
---
+++ OF.Infrastructure.Auth/Extensions.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using OF.Auth.Core;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace OF.Infrastructure.Auth
+{
+
+    public static class Extensions
+    {
+        public static string GenerateJWT(this AppUser user, string SecretKey, int tokenMinutes)
+        {
+            return GenerateJWT(user.ID.ToString(), SecretKey, tokenMinutes);
+        }
+        public static string GenerateJWT(this AppUser user, string AppID, string SecretKey, int tokenMinutes)
+        {
+            var session = new Dictionary<string, object>();
+            session.Add("id", user.ID);
+            session.Add("appId", AppID);
+            session.Add("roles", string.Join(",", user.UserRoles.Select(x=>x.Code)));
+            return GenerateJWT(session, SecretKey, tokenMinutes);
+        }
+
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = Encoding.GetEncoding(28591).GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static bool IsBase64(this string base64String)
+        {
+            // Credit: oybek https://stackoverflow.com/users/794764/oybek
+            if (string.IsNullOrEmpty(base64String) || base64String.Length % 4 != 0
+               || base64String.Contains(" ") || base64String.Contains("\t") || base64String.Contains("\r") || base64String.Contains("\n"))
+                return false;
+
+            try
+            {
+                Convert.FromBase64String(base64String);
+                return true;
+            }
+            catch (Exception exception)
+            {
+                // Handle the exception
+                System.Diagnostics.Trace.TraceError(exception.Message);
+                System.Diagnostics.Trace.TraceError(exception.StackTrace);
+            }
+            return false;
+        }
+
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.GetEncoding(28591).GetString(base64EncodedBytes);
+        }
+
+        public static string CreateMD5(this string input)
+        {
+            // Use input string to calculate MD5 hash
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes;
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create()) {
+                hashBytes = md5.ComputeHash(inputBytes);
+            } ;
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        private static string GenerateJWT(string Id, string SecretKey, int tokenMinutes)
+        {
+            // generate token that is valid for 45 minutes
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", Id) }),
+                Expires = DateTime.UtcNow.AddMinutes(tokenMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        private static string GenerateJWT(Dictionary<string, object> valuePairs, string SecretKey, int tokenMinutes)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(SecretKey);
+            var identity = valuePairs.Select(x => new Claim(x.Key, $"{x.Value}"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(identity),
+                Expires = DateTime.UtcNow.AddMinutes(tokenMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+}
