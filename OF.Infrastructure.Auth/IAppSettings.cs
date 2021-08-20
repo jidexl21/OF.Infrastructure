ba03ff63e167688970e77f74ebff85be92b5a161
jidexl21 <jidexl21@gmail.com>
2021-08-20 07:04:58 +0100

    Added specifications for Integration

---
+++ OF.Infrastructure.Auth/IAppSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OF.Infrastructure.Auth
+{
+    public interface IAppSettings
+    {
+        /// <summary>
+        /// Secret JWT Key
+        /// </summary>
+        string Key { get; set; }
+        /// <summary>
+        /// Application Identifier
+        /// </summary>
+        string AppID { get; set; }
+        /// <summary>
+        /// URL of the authentication endpoint
+        /// </summary>
+        string AuthAppURL { get; set; }
+    }
+}
