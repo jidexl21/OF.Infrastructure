b6ba43c06eefee4e36a8c74b3dd41f34185c247d
jidexl21 <jidexl21@gmail.com>
2022-01-26 14:40:29 +0100

    Added Auth Service Spec to Infrastructure

---
+++ OF.Infrastructure.Auth/IAuthUserService.cs
@@ -0,0 +1,13 @@
+﻿using OF.Auth.Core;
+using System.Threading.Tasks;
+
+namespace OF.Infrastructure.Auth.Services
+{
+    public interface IAuthUserService
+    {
+        Task<IAppUser> GetById(long userID);
+        Task<IAppUser> GetById(string AppId, long userID);
+        Task<IAppUser> GetByToken(string token);
+
+    }
+}
